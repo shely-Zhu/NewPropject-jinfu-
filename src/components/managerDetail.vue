3aabb08ae756da1678a372b6e03cc5d84298e6a1
bashuang <53343306+bashuang@users.noreply.github.com>
2020-05-26 20:36:21 +0800

    理财-理财师详情页面布局

--- src/components/managerDetail.vue
+++ src/components/managerDetail.vue
@@ -5,35 +5,113 @@
  -->
 <template>
   <div class="manager-box">
+    <!-- 标题 -->
+    <div id="pageTitle"></div>
     <!-- 卡片 -->
     <div class="card-box">
       <div class="card-wrap">
+        <!-- 头像 -->
         <div class="head-img-wrap">
           <img class="head-img" v-if="false" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg" alt="">
           <span class="head-img nomorl" v-else>白</span>
         </div>
+        <!-- 右侧个人信息 -->
+        <div class="right-name">白鹭资管</div>
+        <div class="introduce">有近十年金融</div>
+        <!-- 虚线间隔线 -->
+        <img class="dashed-line" src="../assets/img/dashed_line.png" alt="">
+        <!-- 下---名片信息 -->
+        <ul class="card-message">
+          <li class="message message1">备案：111</li>
+          <li class="message message2">成立日期：222</li>
+          <li class="message message3">管理规模：333</li>
+          <li class="message message4">核心策略：444</li>
+        </ul>
       </div>
     </div>
-    <!-- 标签列表 -->
-    <div class="switch-tab">
-
+    <!-- 标签列表tab -->
+    <div class="switch-tab-wrap">
+      <!-- 用于滚动，没有实际意义 -->
+      <div ref="touchstone" id="touchstone"></div>
+      <!-- 顶部标题 -->
+      <div :class="[isSticky ? 'switch-tab-sticky stickyTab' : 'switch-tab-sticky']">
+        <mt-tabbar v-model="active" :class="isFixed ? 'switch-tab fixed-Tab' : 'switch-tab'">
+          <mt-tab-item id="manager-tab1" @click.native="changeActive('manager-tab1')">投资策略</mt-tab-item>
+          <mt-tab-item id="manager-tab2" @click.native="changeActive('manager-tab2')">研究报告</mt-tab-item>
+        </mt-tabbar>
+      </div>
+      <!-- 分类内容 -->
+      <mt-tab-container v-model="active">
+        <mt-tab-container-item id="manager-tab1" class="containt-left">
+          内容
+        </mt-tab-container-item>
+        <mt-tab-container-item id="manager-tab2" class="containt-right">
+          <div class="report-list" v-for="(item,index) in reportList" :key="index">
+            <div class="report-img"><img :src="item.img" alt=""></div>
+            <div class="report-tit">{{item.title}}</div>
+            <div class="report-time">{{item.time}}</div>
+          </div>
+        </mt-tab-container-item>
+      </mt-tab-container>
     </div>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
-     
-    }
+      active:'manager-tab1',
+      reportList:[
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},
+        // {img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'},{img:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3689483593,2509482904&fm=26&gp=0.jpg',title:'标题肯德基拉升的kjsghkhgkjsdkjfkdslhgkjdhsg寒暑假了快递费监考老师开封府几十块两地分居聊点啥',time:'2020-09-09'}
+      ],
+      isFixed:false,//是否固定tab的类名
+      isSticky:false, //是否粘性定位的class
+    };
   },
   components: {},
   mounted() {
-    
+    //判断苹果和安卓
+    if( window.appIsIOS ){
+      this.isSticky = true;
+    }
+    window.addEventListener('scroll', this.handleScroll, true);
+  },
+  // 销毁滚动实例
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll, true);
   },
   methods: {
-   
+    // 点击标签
+    changeActive(active){
+      this.isFixed = false;
+      this.isSticky = false;
+    },
+    // 滚动监听事件
+    handleScroll(){
+      let height = document.getElementById('pageTitle').clientHeight;
+        if (this.$refs.touchstone.getBoundingClientRect().top <= height ) {
+            this.isFixed = true;
+        } else {
+            this.isFixed = false;
+        }
+      //判断苹果和安卓
+      if( !window.appIsIOS ){
+        //安卓手机，可以使用这种办法
+        let height = document.getElementById('pageTitle').clientHeight;
+        if (this.$refs.touchstone.getBoundingClientRect().top <= height ) {
+            this.isFixed = true;
+        } else {
+            this.isFixed = false;
+        }
+      }
+    },
   }
 };
 </script>
@@ -43,6 +121,7 @@ export default {
   width: 100%;
   height: 100%;
   background-color: #F3F5F7;
+  overflow: auto;
   .card-box{
     width: 100%;
     height: auto;
@@ -68,18 +147,187 @@ export default {
       line-height: 3.625rem;
       font-size:2.15rem;
       font-weight:600;
+      .head-img{
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+      }
+      .nomorl{
+        color:#ededed;
+      }
     }
-    .head-img{
-      display: inline-block;
+    /* 右边姓名和简介介绍 */
+    .right-name{
+      position: absolute;
+      top:1.7rem;
+      left:5.35rem;
+      height: 1rem;
+      font-size:.7rem;
+      font-weight:600;
+      color:rgba(255,255,255,1);
+      line-height:1rem;
+      background:linear-gradient(161deg, rgba(251,226,189,1) 0%, rgba(226,181,128,1) 100%);
+      -webkit-background-clip:text;
+      -webkit-text-fill-color:transparent;
+    }
+    .introduce{
+      width: 10.9rem;
+      height: 1.875rem;
+      position: absolute;
+      top:2.95rem;
+      left:5.3rem;
+      font-size:.6rem;
+      font-weight:400;
+      color:rgba(255,255,255,1);
+      line-height:.9rem;
+      opacity: 0.68;
+      overflow: hidden;
+    }
+    /* 虚线 */
+    .dashed-line{
+      position: absolute;
+      top:5.75rem;
+      left:.825rem;
+      width: 15.75rem;
+      height: .025rem;
+
+    }
+    /* 下部分名片信息 */
+    .card-message{
       width: 100%;
-      height: 100%;
-    }
-    .nomorl{
-      color:rgba(0,0,0,1);
-      opacity: 0.6;
+      position: absolute;
+      top:6.575rem;
+      left:0;
+      padding:0 .675rem 0 .825rem;
+      display: flex;
+      flex-wrap:wrap;
+      .message{
+        height:1.375rem;
+        font-size:.6rem;
+        font-weight:400;
+        color:rgba(255,255,255,1);
+        line-height:.825rem;
+        opacity: 0.7;
+        width: 8.5rem;
+      }
+      .message2,.message4{
+        width: 7.15rem;
+      }
     }
   }
-
-
+  /* tab标签 */
+  .switch-tab-wrap{
+    width: 100%;
+    height: auto;
+    position: relative;
+    overflow: auto;
+    .switch-tab-sticky{
+      width: 100%;
+      height: 2rem;
+      position: relative;
+      .switch-tab{
+      position: absolute;
+      left:0;
+      top:0;
+      height: 2rem;
+      font-size:.7rem;
+      font-weight:600;
+      z-index:11;
+      background: #FFFFFF;
+      .mint-tab-item-label{
+        height: 1rem;
+        line-height: 1rem;
+      }
+      /* 未点击标签 */
+      .mint-tab-item{
+        padding-top:.45rem;
+        color: #9B9B9B;
+        font-weight: 400;
+      }
+      /* 点击标签 */
+      .mint-tab-item.is-selected{
+        color: #272727;
+        padding-top:.45rem;
+        background: url('../assets/img/erji.png') no-repeat center bottom 0.1px;
+        background-size: 1.25rem .15rem
+      }
+    }
+    .fixed-Tab{
+      width: 100%;
+      position: fixed;
+      left:0;
+      top:2.2rem;
+      z-index:12;
+    }
+  }
+    /* 粘性定位class */
+    .stickyTab{
+      position: -webkit-sticky;
+      position: sticky;
+      top: 0;
+      z-index: 2;
+    }
+    /* 标签对应的内容 */
+    /* ---左--- */
+    .containt-left{
+      background: #FFFFFF;
+      padding:.95rem 1.475rem .925rem 1.5rem;
+      color:#676767;
+      font-weight:400;
+      line-height:1.25rem;
+      font-size: .7rem;
+      word-break:break-all;
+      word-wrap:break-word;
+      overflow: auto;
+    }
+    /* ---右--- */
+    .containt-right{
+      padding:.425rem 1.55rem 1.65rem .725rem;
+      background: #FFFFFF;
+      .report-list{
+        padding-top:1.175rem;
+        position: relative;
+        .report-img{
+          width: 5.75rem;
+          height: 4.025rem;
+          border-radius: .2rem;
+          overflow: hidden;
+          img{
+            display: inline-block;
+            width: 100%;
+            height: 100%;
+          }
+        }
+        .report-tit{
+          position: absolute;
+          left:6.5rem;
+          top:1.075rem;
+          height: 2.5rem;
+          width: 9.9rem;
+          color: #272727;
+          font-size:.8rem;
+          font-weight:600;
+          color:rgba(39,39,39,1);
+          line-height:1.25rem;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          overflow: hidden;
+          word-break:break-all;
+          word-wrap:break-word
+        }
+        .report-time{
+          height: .925rem;
+          color: #9B9B9B;
+          font-size:.65rem;
+          font-weight:400;
+          line-height:.925rem;
+          position: absolute;
+          left:6.5rem;
+          top:4.275rem;
+        }
+      }
+    }
+  }
 }
 </style>
